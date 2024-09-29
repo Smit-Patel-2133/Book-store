b96ebb05ead9206d7f2b1ea3b7b48dab218a7605
smit patel 2133 <smitnpatel013@gmail.com>
2024-09-29 11:31:32 +0530

    auth

---
+++ Front-End/src/features/authentication/auth.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import def_pic from "../../assets/images/profile/1.png";
+
+const initialState = {
+    auth: {
+        name: '',
+        email: "",
+        isLogedin: false,
+        profilePicture: null // Initialize profile picture as null
+    }
+};
+
+export const userDetails = createSlice({
+    name: 'user_info',
+    initialState,
+    reducers: {
+        currentUser: (state, action) => {
+            const { name, email, profilePicture } = action.payload;
+
+            state.auth = {
+                name: name,
+                email: email,
+                isLogedin: !!name,
+                profilePicture: profilePicture // Set profile picture from action payload
+            };
+        }
+    }
+});
+
+export const { currentUser } = userDetails.actions;
+
+export default userDetails.reducer;
